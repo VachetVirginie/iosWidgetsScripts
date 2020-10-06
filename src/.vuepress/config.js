520c56f2ddb19b51235756fc5f1cc9ea3fd5ce88
Vivi <vachetvirginie@gmail.com>
2020-10-06 16:04:04 +0200

    fix menu

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -40,10 +40,6 @@ module.exports = {
       {
         text: 'Scripts',
         link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
       }
     ],
     sidebar: {
